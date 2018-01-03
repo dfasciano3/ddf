0624366654af0e79fffd8ae78f58ce29cade31cc
Domenico <d.fasciano2@studenti.uniba.it>
2018-01-03 20:16:36 +0100

    zssd

---
+++ src/net/yura/domination/engine/RiskControllerException.java
@@ -0,0 +1,8 @@
+package net.yura.domination.engine;
+
+public class RiskControllerException extends Exception {
+
+    public RiskControllerException() { }
+
+    public RiskControllerException(String s) { super(s); }
+}
